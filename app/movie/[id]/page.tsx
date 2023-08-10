8328eecc51cc8e731e355d392de0a1f3edc142a2
Jonathan <jfangon2002@gmail.com>
2023-08-09 18:22:01 -0700

    most of the app finished - needs error handling before deployment

--- app/movie/[id]/page.tsx
+++ app/movie/[id]/page.tsx
@@ -1,37 +1,61 @@
-import Image from 'next/image';
-import { Movie } from '@/app/interfaces';
+export const dynamic = 'force-dynamic';
+
+import SubmitButton from '@/app/components/SubmitButton';
+import { db } from '@/app/db';
+import { revalidatePath } from 'next/cache';
 
 async function getData(id: string) {
-  const url = await fetch(
-    `https://api.themoviedb.org/3/movie/${id}?language=en-US`,
-    {
-      headers: {
-        accept: 'application/json',
-        Authorization: process.env.MOVIEDB_API as string,
-      },
-      next: {
-        revalidate: 60,
-      },
-    }
-  );
+  const data = db.comment.findMany({
+    where: {
+      movieId: id,
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+  });
 
-  return url.json();
+  return data;
 }
 
-export default async function MovieId({ params }: { params: { id: string } }) {
-  const data: Movie = await getData(params.id);
+async function postData(formData: FormData) {
+  'use server';
+
+  const data = await db.comment.create({
+    data: {
+      message: formData.get('comment') as string,
+      movieId: formData.get('id') as string,
+    },
+  });
+
+  revalidatePath('/movie/[id]');
+}
+
+export default async function Page({ params }: { params: { id: string } }) {
+  const data = await getData(params.id);
 
   return (
-    <div className='min-h-screen p-10'>
-      <div className='h-[40vh] relative'>
-        <Image
-          src={`https://image.tmdb.org/t/p/original/${data.backdrop_path}`}
-          alt='movie image'
-          className='object-cover w-full rounded-lg'
-          fill
-        />
+    <div className='rounded-lg border p-3'>
+      <h1 className='text-xl font-semibold mb-6'>Your Opinion</h1>
+
+      <div>
+        <form action={postData}>
+          <textarea
+            name='comment'
+            className='w-full border border-teal-500 rounded-lg p-2'
+            placeholder='Add comment here...'
+          ></textarea>
+          <input type='hidden' name='id' value={params.id} />
+          <SubmitButton />
+        </form>
+
+        <div className='mt-5 flex flex-col gap-y-3'>
+          {data.map((post) => (
+            <div key={post.id}>
+              <p>{post.message}</p>
+            </div>
+          ))}
+        </div>
       </div>
-      <h1 className='text-4xl font-bold text-center pt-5'>{data.title}</h1>
     </div>
   );
 }
--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ['image.tmdb.org'],
   },
+  experimental: {
+    serverActions: true,
+  },
 };
 
 module.exports = nextConfig;
