e79b85b6eb207ceed70467c56d75d430d5bec552
Jonathan <jfangon2002@gmail.com>
2023-08-24 19:06:44 -0700

    layout changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,8 +18,8 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={`${inter.className} bg-gray-100 dark:bg-gray-900`}>
-        <nav className='px-10 pt-5'>
-          <Link href='/' className='text-2xl font-semibold'>
+        <nav className='px-10 pt-10 flex justify-center'>
+          <Link href='/' className='text-4xl font-semibold'>
             <span className='dark:text-gray-200'>Movie</span>
             <span className='text-teal-400 dark:text-teal-300'>Critic</span>
           </Link>
